d54826760807860362bd7adf87bdd7f001af5983
Masashi Kishimoto <mkishimoto0716@gmail.com>
2021-02-26 15:01:00 +0900

    Expedited jobsを試す
    ただし、https://issuetracker.google.com/issues/180884673#comment2によりアプリがクラッシュするので、2.7.0-alpha02待ち

--- samples/ForegroundServiceLaunchRestrictionsSample/app/src/main/java/com/github/mag0716/foregroundservicelaunchrestrictionssample/MainActivity.kt
+++ samples/ForegroundServiceLaunchRestrictionsSample/app/src/main/java/com/github/mag0716/foregroundservicelaunchrestrictionssample/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.WorkManager
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+/**
+ * FIXME：https://issuetracker.google.com/issues/180884673#comment2 により WorkManager 2.7.0-alpha01 に依存するとアプリが起動できなくなる
+ */
 class MainActivity : AppCompatActivity() {
 
     companion object {
@@ -15,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var startForegroundServiceButton: Button
+    private lateinit var startWorkerButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +28,10 @@ class MainActivity : AppCompatActivity() {
         startForegroundServiceButton = findViewById(R.id.startForegroundServiceButton)
         startForegroundServiceButton.setOnClickListener {
             startForegroundService()
+        }
+        startWorkerButton = findViewById(R.id.startWorkerButton)
+        startWorkerButton.setOnClickListener {
+            startWorker()
         }
     }
 
@@ -37,5 +46,13 @@ class MainActivity : AppCompatActivity() {
                 Log.w(TAG, "failed startForegroundService", exception)
             }
         }
+    }
+
+    private fun startWorker() {
+        val workRequest = LoggingWorker.createWorkRequest(5000)
+        val workManager = WorkManager.getInstance(this)
+        workManager.cancelAllWork()
+        workManager.beginWith(workRequest)
+            .enqueue()
     }
 }
