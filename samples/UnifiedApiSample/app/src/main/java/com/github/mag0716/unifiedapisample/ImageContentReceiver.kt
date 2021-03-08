dd3f6b49e69dc8e55e5b62837a9fa951ac318e9a
Masashi Kishimoto <mkishimoto0716@gmail.com>
2021-03-08 21:39:00 +0900

    受け取った画像をEditText内に表示する処理を実装

--- samples/UnifiedApiSample/app/src/main/java/com/github/mag0716/unifiedapisample/ImageContentReceiver.kt
+++ samples/UnifiedApiSample/app/src/main/java/com/github/mag0716/unifiedapisample/ImageContentReceiver.kt
@@ -1,7 +1,11 @@
 package com.github.mag0716.unifiedapisample
 
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import androidx.core.view.ContentInfoCompat
 import androidx.core.view.OnReceiveContentListener
 
@@ -24,9 +28,28 @@ class ImageContentReceiver : OnReceiveContentListener {
             for (i in 0 until clipData.itemCount) {
                 val uri = clipData.getItemAt(i).uri
                 Log.d(TAG, "onReceiveContent : $uri")
+                if (view is EditText) {
+                    insertImage(view, uri)
+                }
             }
         }
 
         return remaining
+    }
+
+    private fun insertImage(editText: EditText, uri: Uri) {
+        val context = editText.context
+        val bitmap = MediaStore.Images.Media.getBitmap(
+            context.contentResolver,
+            uri
+        )
+        val drawable = BitmapDrawable(context.resources, bitmap)
+        Log.d(TAG, "insertImage : $uri, $drawable")
+        editText.setCompoundDrawablesWithIntrinsicBounds(
+            null,
+            drawable,
+            null,
+            null
+        )
     }
 }
