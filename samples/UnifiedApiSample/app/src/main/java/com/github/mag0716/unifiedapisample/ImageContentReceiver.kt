35523b3df2a9b3a7f1011c5633b077aa31298e9b
Masashi Kishimoto <mkishimoto0716@gmail.com>
2021-03-12 21:07:45 +0900

    バックグラウンドスレッドで画像取得処理が呼び出されるように対応

--- samples/UnifiedApiSample/app/src/main/java/com/github/mag0716/unifiedapisample/ImageContentReceiver.kt
+++ samples/UnifiedApiSample/app/src/main/java/com/github/mag0716/unifiedapisample/ImageContentReceiver.kt
@@ -1,5 +1,7 @@
 package com.github.mag0716.unifiedapisample
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
@@ -10,6 +12,7 @@ import android.view.View
 import android.widget.EditText
 import androidx.core.view.ContentInfoCompat
 import androidx.core.view.OnReceiveContentListener
+import kotlinx.coroutines.*
 
 class ImageContentReceiver : OnReceiveContentListener {
 
@@ -17,6 +20,16 @@ class ImageContentReceiver : OnReceiveContentListener {
         private const val TAG = "ImageContentReceiver"
         val MIME_TYPES = arrayOf("image/*")
     }
+
+    private val scope = CoroutineScope(
+        SupervisorJob() +
+                Dispatchers.Main +
+                CoroutineExceptionHandler { _, throwable ->
+                    Log.w(
+                        "ImageContentReceiver",
+                        throwable
+                    )
+                })
 
     override fun onReceiveContent(view: View, payload: ContentInfoCompat): ContentInfoCompat? {
         val split = payload.partition {
@@ -40,28 +53,36 @@ class ImageContentReceiver : OnReceiveContentListener {
     }
 
     private fun insertImage(editText: EditText, uri: Uri) {
-        val context = editText.context
-        val contentResolver = context.contentResolver
-        val bitmap = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
-            @Suppress("DEPRECATION")
-            MediaStore.Images.Media.getBitmap(
-                contentResolver,
-                uri
+        scope.launch {
+            val context = editText.context
+            val bitmap = loadImage(context, uri)
+            val drawable = BitmapDrawable(context.resources, bitmap)
+            editText.setCompoundDrawablesWithIntrinsicBounds(
+                null,
+                drawable,
+                null,
+                null
             )
-        } else {
-            ImageDecoder.decodeBitmap(
-                ImageDecoder.createSource(
+        }
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext") // 実際にはバックグラウンドスレッドで実行されるのがwarningが出てしあうので抑制する
+    private suspend fun loadImage(context: Context, uri: Uri): Bitmap =
+        withContext(Dispatchers.IO) {
+            val contentResolver = context.contentResolver
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+                @Suppress("DEPRECATION")
+                MediaStore.Images.Media.getBitmap(
                     contentResolver,
                     uri
                 )
-            )
+            } else {
+                ImageDecoder.decodeBitmap(
+                    ImageDecoder.createSource(
+                        contentResolver,
+                        uri
+                    )
+                )
+            }
         }
-        val drawable = BitmapDrawable(context.resources, bitmap)
-        editText.setCompoundDrawablesWithIntrinsicBounds(
-            null,
-            drawable,
-            null,
-            null
-        )
-    }
 }
